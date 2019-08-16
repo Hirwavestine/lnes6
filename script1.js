3088d0160000353258958c44c4c740fc2de3d5aa
hirwavestine <gilhirwa22@gmail.com>
2019-08-16 11:18:19 +0200

    add default parameters

--- script1.js
+++ script1.js
@@ -1,3 +1,8 @@
-let getId = _ => 123;
+let trackCar = function(cardId, city = "NY") {
+  console.log(`Tracking ${cardId} in ${city}.`);
+};
 
-console.log(getId());
+console.log(trackCar(123));
+//Tracking 123.
+console.log(trackCar(123, "Chicago"));
+//Tracking 123 in Chicago.
