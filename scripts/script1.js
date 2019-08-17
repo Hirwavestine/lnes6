b3539351d2c943b4a92436fb65445513cc61dc0e
hirwavestine <gilhirwa22@gmail.com>
2019-08-17 17:39:26 +0200

    erreor handling with try and catch

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,5 +1,9 @@
-//Modify the DOM
-
-let element = document.getElementById("first");
-element.classList.add("p2");
-console.log(element);
+//the code we know has error
+try {
+  let car = "newCar";
+  //catch the error in the code, this only execute if an error is throw in our code block
+} catch (error) {
+  console.log("error:", error);
+}
+//the code will continue to execute after catching the errors
+console.log("continuing");
