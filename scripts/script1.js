1d00c4c9fc0e56c6eb9c222444f1cd289d60a81f
hirwavestine <gilhirwa22@gmail.com>
2019-08-17 18:57:23 +0200

    settling a promise

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,4 +1,7 @@
 let promise = new Promise(function(resolve, reject) {
   setTimeout(resolve, 100, "someValue");
 });
-console.log(promise);
+promise.then(
+  value => console.log("fulfilled: " + value),
+  error => console.log("rejected: " + error)
+);
