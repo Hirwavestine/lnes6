727c7224295bb176100c9e4c2a1b30cc21cdec4c
hirwavestine <gilhirwa22@gmail.com>
2019-08-17 18:20:00 +0200

    Developer create their own error unless of created by javascript

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,9 +1,10 @@
 //the code we know has error
 try {
-  let car = "newCar";
-  //catch the error in the code, this only execute if an error is throw in our code block
+  throw new Error("my custom error");
 } catch (error) {
+  //catch the error in the code, this only execute if an error is throw in our code block
   console.log("error:", error);
 } finally {
-  console.log("continuing");
+  //execute some code regardless wether or not error was thrown
+  console.log("this will always execute");
 }
