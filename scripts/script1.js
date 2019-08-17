56d163232e492734383964efbc394b6a1f78498f
hirwavestine <gilhirwa22@gmail.com>
2019-08-17 18:36:53 +0200

    another way to handle error

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,10 +1,4 @@
-//the code we know has error
-try {
-  throw new Error("my custom error");
-} catch (error) {
-  //catch the error in the code, this only execute if an error is throw in our code block
-  console.log("error:", error);
-} finally {
-  //execute some code regardless wether or not error was thrown
-  console.log("this will always execute");
-}
+let promise = new Promise(function(resolve, reject) {
+  setTimeout(resolve, 100, "someValue");
+});
+console.log(promise);
