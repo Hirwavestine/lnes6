9005859c1211d63dbacdd2e608ccc94953a449ac
hirwavestine <gilhirwa22@gmail.com>
2019-08-19 09:43:15 +0200

    posting from javascript

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,12 +1,21 @@
+import $ from "jquery";
+
 let form = document.getElementById("user-form");
+
 form.addEventListener("submit", event => {
   let user = form.elements["user"];
-  let userError = document.getElementById("user-error");
+  let avatarFile = form.elements["avatar-file"];
 
-  userError.textContent = "Invalid entry";
-  userError.style.color = "red";
-  userError.style.borderColor = "red";
-  user.focus();
+  let posting = {
+    user: user.value,
+    avatarFile: avatarFile.value
+  };
+
+  let promise = $.post("http://---/api/v1/users", posting);
+  promise.then(
+    data => console.log("success: ", data),
+    error => console.log("error: ", error)
+  );
 
   event.preventDefault();
 });
