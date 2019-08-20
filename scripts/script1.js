535b480145e84660723fee74e2bdf5012d54f49d
hirwavestine <gilhirwa22@gmail.com>
2019-08-20 10:45:17 +0200

    using the writable attribute

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,11 +1,14 @@
 "use strict";
 
 var cat = {
-  name: "fluffy",
+  name: { first: "fluffy", last: "laBeouf" },
   color: "white"
 };
 //using the writable attribute
 Object.defineProperty(cat, "name", { writable: false });
-//this throw an error because you make property name unwritable
-cat.name = "pop";
+//use this to make an object which has an other object unchanged
+Object.freeze(cat.name);
+//this can not throw an error because you set a name property to an object
+
+cat.name.first = "crack";
 display(Object.getOwnPropertyDescriptor(cat, "name"));
