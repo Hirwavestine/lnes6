0630f4894ad99c4e0a1ec7030833c9a170c622b4
hirwavestine <gilhirwa22@gmail.com>
2019-08-20 09:58:01 +0200

    using writable attribute

--- scripts/script1.js
+++ scripts/script1.js
@@ -4,4 +4,8 @@ var cat = {
   name: "fluffy",
   color: "white"
 };
+//using the writable attribute
+Object.defineProperty(cat, "name", { writable: false });
+//this throw an error because you make property name unwritable
+cat.name = "pop";
 display(Object.getOwnPropertyDescriptor(cat, "name"));
