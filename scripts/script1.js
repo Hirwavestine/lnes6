7e2e934663ce15f4b278c9d3d494590730455bbe
hirwavestine <gilhirwa22@gmail.com>
2019-08-21 16:06:19 +0200

    changing a function prototype

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,23 +1,22 @@
 "use strict";
 
-"use strict";
-
 function Cat(name, color) {
   this.name = name;
   this.color = color;
 }
 Cat.prototype.age = 4;
 var fluffy = new Cat("Fluffy", "white");
-
-display(fluffy.__proto__);
-display(fluffy.__proto__.__proto__);
-display(fluffy.__proto__.__proto__.__proto__);
-/*
-Cat {
-age: 4
-}
-Object {
-}
-null
-
+var muffin = new Cat("Muffin", "Brown");
+//changing a function's prototype
+Cat.prototype = { age: 5 };
+var snowbell = new Cat("snowbell", "white");
+display(fluffy.age);
+display(muffin.age);
+display(snowbell.age);
+display(Cat.prototype.age);
+/* 
+4
+4
+5
+5
 */
