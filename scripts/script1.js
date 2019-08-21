6feb191ea3901e95b899ddffb42fccfb02c956ff
hirwavestine <gilhirwa22@gmail.com>
2019-08-21 16:21:58 +0200

    instances vs.prototypes properties

--- scripts/script1.js
+++ scripts/script1.js
@@ -11,6 +11,7 @@ var muffin = new Cat("Muffin", "Brown");
 display(fluffy.age);
 display(fluffy.__proto__.age);
 display(Object.keys(fluffy));
+display(fluffy.hasOwnProperty("age"));
 /*
 4
 4
@@ -18,4 +19,5 @@ Array {
 0: name
 1: color
 }
+false
 */
